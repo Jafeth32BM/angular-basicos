552f61dd6853b7a010db3cc5eae6711f8ddae1b1
Jafeth BM <jafethbadillomartinez@gmail.com>
2021-07-01 14:23:44 -0500

    Seccion 6: generar un build y publicarlo en Netlify

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Personaje } from '../interfaces/dbz.interface';
+import { Component} from '@angular/core';
 import { DbzService } from '../services/dbz.service';
 
 @Component({
@@ -7,7 +6,6 @@ import { DbzService } from '../services/dbz.service';
   templateUrl: './personajes.component.html'
 })
 export class PersonajesComponent{
-// @Input('data') personajes: Personaje [] =[];
 get personajes (){
   return this.dbzService.personajes;
 }
